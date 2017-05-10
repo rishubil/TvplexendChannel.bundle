77ada18b23cee5f11a55f748639d81ca409cfeec
Nesswit <rishubil@gmail.com>
2017-05-11 03:39:53 +0900

    Remove Media Option
    It is not static values, depends on tvheadend stream profile.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -163,9 +163,6 @@ def Channel(channelId, container=False, **kwargs):
         items=[
             MediaObject(
                 optimized_for_streaming=True,
-                video_resolution = 1080,
-                video_codec=VideoCodec.H264,
-                audio_codec=AudioCodec.AAC,
                 container=CONTAINER,
                 parts=[
                     PartObject(
